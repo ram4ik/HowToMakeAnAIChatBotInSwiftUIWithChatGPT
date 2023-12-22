05c60309ca599579be33b9574c8d0d7920b49664
Ramill Ibragimov <ribragimov@malwarebytes.com>
2023-12-22 08:18:55 +0200

    Initial Commit

---
+++ HowToMakeAnAIChatBotInSwiftUIWithChatGPT/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HowToMakeAnAIChatBotInSwiftUIWithChatGPT
+//
+//  Created by test on 22.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HowToMakeAnAIChatBotInSwiftUIWithChatGPT/HowToMakeAnAIChatBotInSwiftUIWithChatGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToMakeAnAIChatBotInSwiftUIWithChatGPTApp.swift
+//  HowToMakeAnAIChatBotInSwiftUIWithChatGPT
+//
+//  Created by test on 22.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HowToMakeAnAIChatBotInSwiftUIWithChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
